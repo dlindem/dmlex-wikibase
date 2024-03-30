435a5543e2fd34aa3efd297162640e11853cf6d0
David Lindemann <34068491+dlindem@users.noreply.github.com>
2024-03-30 21:11:47 +0100

    Create dmlex_lexbib.py

---
+++ mappings/dmlex_lexbib.py
@@ -0,0 +1,15 @@
+dmlex_properties = {
+"id": {"prop_nr": "P186", "dtype": "string"},
+"homographNumber": {"prop_nr": "P187", "dtype": "string"},
+"sameAs": {"prop_nr": "P57", "dtype": "url"},
+"listingOrder": {"prop_nr": "P33", "dtype": "string"},
+}
+
+dmlex_classes = {
+"lexicographicResource": "Q100",
+"DefinitionTypeTag": "Q101",
+"InflectedFormTag": "Q102",
+"LabelTag": "Q103",
+"LabelTypeTag": "Q105",
+
+}
